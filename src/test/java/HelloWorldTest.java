550136a984929d1a74a06915119b9c0185995913
Krishanu Das <krishanu.das2@cognizant.com>
2018-02-20 14:28:21 +0530

    added basic test file and travis.yml

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		//HelloWorld helloWorld = new HelloWorld();
+		assertEquals(0, 0);
+	}
+
+}
